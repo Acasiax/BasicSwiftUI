c426956058cce77b9e3bcddf88f55daecf9eab95
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-09-02 23:41:58 +0900

    Initial Commit

---
+++ BasicSwiftUI/BasicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUIApp.swift
+//  BasicSwiftUI
+//
+//  Created by 이윤지 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
